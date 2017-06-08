27493daa09749304d119478b4163bac5a9cdcc17
ALIEN <matthias.moulin@gmail.com>
2017-06-08 22:01:01 +0200

    Added Tutorial 02: Creating a Framework and Window

---
+++ Tutorial 02_Creating a Framework and Window/Engine/graphicsclass.cpp
@@ -0,0 +1,47 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: graphicsclass.cpp
+////////////////////////////////////////////////////////////////////////////////
+#include "graphicsclass.h"
+
+
+GraphicsClass::GraphicsClass()
+{
+}
+
+
+GraphicsClass::GraphicsClass(const GraphicsClass& other)
+{
+}
+
+
+GraphicsClass::~GraphicsClass()
+{
+}
+
+
+bool GraphicsClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
+{
+
+	return true;
+}
+
+
+void GraphicsClass::Shutdown()
+{
+
+	return;
+}
+
+
+bool GraphicsClass::Frame()
+{
+
+	return true;
+}
+
+
+bool GraphicsClass::Render()
+{
+
+	return true;
+}
---
+++ Tutorial 02_Creating a Framework and Window/Engine/graphicsclass.h
@@ -0,0 +1,44 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: graphicsclass.h
+////////////////////////////////////////////////////////////////////////////////
+#ifndef _GRAPHICSCLASS_H_
+#define _GRAPHICSCLASS_H_
+
+
+//////////////
+// INCLUDES //
+//////////////
+#include <windows.h>
+
+
+/////////////
+// GLOBALS //
+/////////////
+const bool FULL_SCREEN = false;
+const bool VSYNC_ENABLED = true;
+const float SCREEN_DEPTH = 1000.0f;
+const float SCREEN_NEAR = 0.1f;
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Class name: GraphicsClass
+////////////////////////////////////////////////////////////////////////////////
+class GraphicsClass
+{
+public:
+	GraphicsClass();
+	GraphicsClass(const GraphicsClass&);
+	~GraphicsClass();
+
+	bool Initialize(int, int, HWND);
+	void Shutdown();
+	bool Frame();
+
+private:
+	bool Render();
+
+private:
+
+};
+
+#endif
